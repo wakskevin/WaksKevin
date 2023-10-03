e5fb304da7106538cc3e873259fa095a948a70ee
WaksKevin <wakskevin@outlook.com>
2023-10-03 08:39:55 +0300

    updates url endpoints

--- wakskevin/settings.py
+++ wakskevin/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "WaksKevin/static/"
 
 STATIC_ROOT = BASE_DIR / "static"
 
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [
 # Media files
 # https://docs.djangoproject.com/en/4.2/ref/settings/#media-files
 
-MEDIA_URL = "media/"
+MEDIA_URL = "WaksKevin/media/"
 
 MEDIA_ROOT = BASE_DIR / "media"
 
